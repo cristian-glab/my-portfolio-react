b50f2f29773f13a684a4a4174f1d7e18b48e796e
cristian-glab <cristian.pinzon@gracialab.com.co>
2023-10-22 10:37:49 -0500

    Add technologies section in desktop size

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.scss';
 import './styles.scss';
 import Hero from './components/hero/Hero';
+import Technologies from './components/technologies/Technologies';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Technologies/>
     </div>
   );
 }
--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import './Technologies.scss'
 
 export default class Technologies extends Component {
   render() {
 	return (
-	  <div>Technologies</div>
+	  <section className='technologies'>
+		  <div className='element'>
+        <h1>HTML</h1>
+        <p>4 Years Experience</p>
+      </div>
+      <div className='element'>
+        <h1>CSS</h1>
+        <p>4 Years Experience</p>
+      </div>
+      <div className='element'>
+        <h1>Javascript</h1>
+        <p>4 Years Experience</p>
+      </div>
+      <div className='element'>
+        <h1>Accessibility</h1>
+        <p>4 Years Experience</p>
+      </div>
+      <div className='element'>
+        <h1>React</h1>
+        <p>3 Years Experience</p>
+      </div>
+      <div className='element'>
+        <h1>Sass</h1>
+        <p>3 Years Experience</p>
+      </div>
+		</section>
 	)
   }
 }
